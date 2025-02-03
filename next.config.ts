266916b0352e86c419e0520e4d98d67f0c9eb74f
escobar2025 <eseweb100@gmail.com>
2025-02-03 15:08:29 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+}
+
+module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
 module.exports = {
   typescript: {
     ignoreBuildErrors: true

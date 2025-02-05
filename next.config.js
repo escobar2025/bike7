495305a5dd8f4fcee7838929766c3f2a33d88031
escobar2025 <eseweb100@gmail.com>
2025-02-05 15:22:26 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,34 +1,3 @@
-const nextConfig = {
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true
-  }
-}
-
-module.exports = nextConfig
-
 module.exports = {
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  experimental: {
-    appDir: true
-  }
+  swcMinify: true
 }
-
-module.exports = {
-  typescript: {
-    ignoreBuildErrors: true
-  }
-}
-
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
